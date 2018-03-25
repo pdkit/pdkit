ce89c2a6f03dc4daf3fc73610cf086f151ac14a2
Juan Saez <joan@retechnica.com>
2018-03-25 17:06:43 +0100

    improves autogenerated docs

--- pdkit/tremor_processor.py
+++ pdkit/tremor_processor.py
@@ -5,44 +5,62 @@ import logging
 import numpy as np
 import pandas as pd
 from scipy import interpolate, signal, fft
-from .utils import load_data
-
-NANOSEC_TO_SEC = 1000000000.0
-SAMPLING_FREQUENCY = 100.0  # this was recommended by the author of the pilot study [1]
-CUTOFF_FREQUENCY_HIGH = 2.0  # Hz as per [1]
-FILTER_ORDER = 2  # as per [1]
-WINDOW = 256  # this was recommended by the author of the pilot study [1]
-LOWER_FREQUENCY_TREMOR = 2.0  # Hz as per [1]
-UPPER_FREQUENCY_TREMOR = 10.0  # Hz as per [1]
+from utils import load_data
 
 
 class TremorProcessor:
     '''
-       Tremor Processor Class
+       This is the main Tremor Processor class. Once the data is loaded it will be
+       accessible at data_frame, where it looks like:
+       data_frame.x, data_frame.y, data_frame.z: x, y, z components of the acceleration
+       data_frame.index is the datetime-like index
+       
+       This values are recommended by the author of the pilot study [1]
+       
+       sampling_frequency = 100.0Hz
+       cutoff_frequency = 2.0Hz
+       filter_order = 2
+       window = 256
+       lower_frequency = 2.0Hz
+       upper_frequency = 10.0Hz
+
+       [1] Developing a tool for remote digital assessment of Parkinson s disease
+            Kassavetis	P,	Saifee	TA,	Roussos	G,	Drougas	L,	Kojovic	M,	Rothwell	JC,	Edwards	MJ,	Bhatia	KP
+            
+       [2] The use of the fast Fourier transform for the estimation of power spectra: A method based 
+            on time averaging over short, modified periodograms (IEEE Trans. Audio Electroacoust. 
+            vol. 15, pp. 70-73, 1967)
+            P. Welch
     '''
 
-    def __init__(self):
+    def __init__(self, sampling_frequency=100.0, cutoff_frequency=2.0, filter_order=2,
+                 window=256, lower_frequency=2.0, upper_frequency=10.0):
         try:
             self.amplitude = 0
             self.frequency = 0
+
+            self.sampling_frequency = sampling_frequency
+            self.cutoff_frequency = cutoff_frequency
+            self.filter_order = filter_order
+            self.window = window
+            self.lower_frequency = lower_frequency
+            self.upper_frequency = upper_frequency
+
             logging.debug("TremorProcessor init")
 
         except IOError as e:
             ierr = "({}): {}".format(e.errno, e.strerror)
             logging.error("TremorProcessor I/O error %s", ierr)
-            # return 'IOError ' + ierr
 
         except ValueError as verr:
             logging.error("TremorProcessor ValueError ->%s", verr.message)
-            # return 'TremorPRocessor ValueError'
 
         except:
             logging.error("Unexpected error on TremorProcessor init: %s", sys.exc_info()[0])
-            # return 'Unexpected error on TremorProcessor init', sys.exc_info()[0]
 
     def load_data(self, filename, format_file='cloudupdrs'):
         '''
-            General method to load data with different format file
+            This is a general load data method where the format of data to load can be passed as a parameter,
 
             :param str filename: The path to load data from
             :param str format_file: format of the file. Default is CloudUPDRS. Set to mpower for mpower data.
@@ -51,74 +69,71 @@ class TremorProcessor:
         logging.debug("data loaded")
         return load_data(filename, format_file)
 
-    def resample_signal(self, data_frame, sampling_frequency=SAMPLING_FREQUENCY):
+    def resample_signal(self, data_frame):
         '''
-            Resample signal
-            We need to resample the signal as it is recorded with variable sampling rate
+            Convenience method for frequency conversion and resampling of data frame. 
+            Object must have a DatetimeIndex. After re-sampling, this methods interpolate the time magnitude sum 
+            acceleration values and the x,y,z values of the data frame acceleration
 
-            :param data_frame: the data frame    
-            :param str filename: The path to load data from
-            :param str formatfile: format of the file. Default is CloudUPDRS. Set to mpower for mpower data.
+            :param data_frame: the data frame to resample
+            :param str sampling_frequency: the sampling frequency. Default is 100Hz, as recommended by the author of the pilot study [1]
         '''
-        df_resampled = data_frame.resample(str(1 / sampling_frequency) + 'S').mean()
-        # interpolate function
+        df_resampled = data_frame.resample(str(1 / self.sampling_frequency) + 'S').mean()
+
         f = interpolate.interp1d(data_frame.td, data_frame.mag_sum_acc)
-        # use arange to equally space the time difference
-        new_timestamp = np.arange(data_frame.td[0], data_frame.td[-1], 1.0 / sampling_frequency)
-        # interpolate the time magnitude sum acceleration values
+        new_timestamp = np.arange(data_frame.td[0], data_frame.td[-1], 1.0 / self.sampling_frequency)
         df_resampled.mag_sum_acc = f(new_timestamp)
-        # interpolate the x,y,z values of the data frame
+
         logging.debug("resample signal")
         return df_resampled.interpolate(method='linear')
 
-    def filter_signal(self, data_frame, cutoff_frequency=CUTOFF_FREQUENCY_HIGH, filter_order=FILTER_ORDER):
+    def filter_signal(self, data_frame):
         '''
-            Filter signal. High pass filter the signal as per [1]
-            [1] Developing a tool for remote digital assessment of Parkinson s disease
-            Kassavetis	P,	Saifee	TA,	Roussos	G,	Drougas	L,	Kojovic	M,	Rothwell	JC,	Edwards	MJ,	Bhatia	KP
+            This method filters a data frame signal as suggested in [1]. First step is to high pass filter the data
+            frame using a butter Butterworth digital and analog filter 
+            (https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.signal.butter.html). Then the method 
+            filter the data frame along one-dimension using a digital filter. 
+            (https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.lfilter.html)
 
             :param data_frame: the data frame    
-            :param str filename: The path to load data from
-            :param str formatfile: format of the file. Default is CloudUPDRS. Set to mpower for mpower data.
+            :param str cutoff_frequency: The path to load data from
+            :param str filter_order: format of the file. Default is CloudUPDRS. Set to mpower for mpower data.
         '''
-        # first step is to high pass filter the signal as per [1]
-        b, a = signal.butter(filter_order, 2 * cutoff_frequency / SAMPLING_FREQUENCY, 'high', analog=False)
+        b, a = signal.butter(self.filter_order, 2 * self.cutoff_frequency / self.sampling_frequency, 'high', analog=False)
         filtered_signal = signal.lfilter(b, a, data_frame.mag_sum_acc.values)
         data_frame['filtered_signal'] = filtered_signal
+
         logging.debug("filter signal")
         return data_frame
 
-    def fft_signal(self, data_frame, window=WINDOW):
+    def fft_signal(self, data_frame):
         '''
-            FFT signal. Perform fft on the signal using a hanning window
+            This method perform Fast Fourier Transform on the data frame using a hanning window
+            (https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.signal.hann.html)
 
             :param data_frame: the data frame    
             :param str window: hanning window size
         '''
         signal_length = len(data_frame.filtered_signal.values)
-        ll = int ( signal_length / 2 - window / 2 )
-        rr = int ( signal_length / 2 + window / 2 )
+        ll = int ( signal_length / 2 - self.window / 2 )
+        rr = int ( signal_length / 2 + self.window / 2 )
         msa = data_frame.filtered_signal[ll:rr].values
-        hann_window = signal.hann(window)
+        hann_window = signal.hann(self.window)
 
         msa_window = (msa * hann_window)
         transformed_signal = fft(msa_window)
 
         data = {'filtered_signal': msa_window, 'transformed_signal': transformed_signal,
                 'dt': data_frame.td[ll:rr].values}
-        # fft signal is a new data frame
+
         data_frame_fft = pd.DataFrame(data, index=data_frame.index[ll:rr],
                                       columns=['filtered_signal', 'transformed_signal', 'dt'])
         logging.debug("fft signal")
         return data_frame_fft
 
-    def tremor_amplitude(self, data_frame,
-                         lower_frequency=LOWER_FREQUENCY_TREMOR, upper_frequency=UPPER_FREQUENCY_TREMOR):
+    def tremor_amplitude(self, data_frame):
         '''
-            Tremor Amplitude. Extract the fft components and sum the ones from lower to upper freq as per [1]
-            
-            [1] Developing a tool for remote digital assessment of Parkinson s disease
-            Kassavetis	P,	Saifee	TA,	Roussos	G,	Drougas	L,	Kojovic	M,	Rothwell	JC,	Edwards	MJ,	Bhatia	KP
+            This methods extract the fft components and sum the ones from lower to upper freq as per [1]
 
             :param data_frame: the data frame    
             :param str lower_frequency: LOWER_FREQUENCY_TREMOR
@@ -128,38 +143,35 @@ class TremorProcessor:
         normalised_transformed_signal = data_frame.transformed_signal.values / signal_length
 
         k = np.arange(signal_length)
-        T = signal_length / SAMPLING_FREQUENCY
+        T = signal_length / self.sampling_frequency
         frq = k / T  # two sides frequency range
 
         frq = frq[range(int(signal_length / 2))]  # one side frequency range
         ts = normalised_transformed_signal[range(int(signal_length / 2))]
-        self.amplitude = sum(abs(ts[(frq > lower_frequency) & (frq < upper_frequency)]))
+        self.amplitude = sum(abs(ts[(frq > self.lower_frequency) & (frq < self.upper_frequency)]))
         self.frequency = frq[abs(ts).argmax(axis=0)]
+
         logging.debug("tremor amplitude calculated")
 
-    def tremor_amplitude_by_welch(self, data_frame, window=WINDOW, lower_frequency=LOWER_FREQUENCY_TREMOR,
-                                  upper_frequency=UPPER_FREQUENCY_TREMOR):
+    def tremor_amplitude_by_welch(self, data_frame):
         '''
-            Welch Amplitude. Use the Welch method [3] to obtain the power spectral density, this is a robust 
+            This methods uses the Welch method [2] to obtain the power spectral density, this is a robust 
             alternative to using fft_signal & calc_tremor_amplitude
-            
-            [3] The use of the fast Fourier transform for the estimation of power spectra: A method based 
-            on time averaging over short, modified periodograms (IEEE Trans. Audio Electroacoust. 
-            vol. 15, pp. 70-73, 1967)
-            P. Welch
 
             :param data_frame: the data frame    
             :param str lower_frequency: LOWER_FREQUENCY_TREMOR
             :param str upper_frequency: UPPER_FREQUENCY_TREMOR
         '''
-        frq, Pxx_den = signal.welch(data_frame.filtered_signal.values, SAMPLING_FREQUENCY, nperseg=window)
+        frq, Pxx_den = signal.welch(data_frame.filtered_signal.values, self.sampling_frequency, nperseg=self.window)
         self.frequency = frq[Pxx_den.argmax(axis=0)]
-        self.amplitude = sum(Pxx_den[(frq > lower_frequency) & (frq < upper_frequency)])
+        self.amplitude = sum(Pxx_den[(frq > self.lower_frequency) & (frq < self.upper_frequency)])
+
         logging.debug("tremor amplitude by welch calculated")
 
     def process(self, data_frame, method='fft'):
         '''
-            Process method.
+            This methods calculates the tremor amplitude of the data frame. It accepts two different methods,
+            'fft' and 'welch'. First the signal gets re-sampled and then high pass filtered. 
 
             :param data_frame: the data frame    
             :param data_frame: pandas data frame.
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='pdkit',
-    version='0.2.2',
+    version='0.2.3',
     description='Parkinson`s Disease Kit',
     url='https://github.com/pdkit/pdkit',
     long_description=readme(),
