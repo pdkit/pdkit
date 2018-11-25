38f76994bfcb625f692402704a59cf8eff657c41
cosmin <cosmin@dcs.bbk.ac.uk>
2018-11-25 15:50:35 +0000

    added QoIProcessor notebook and extra features

--- pdkit/dl_processor.py
+++ pdkit/dl_processor.py
@@ -2,6 +2,7 @@ import numpy as np
 import random as rn
 
 from pdkit.models import RCL
+from pdkit.utils import window_features
 
 from keras.optimizers import sgd, adam
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
@@ -14,6 +15,7 @@ class QoIProcessor(object):
                  input_shape=(150, 4),
                  labels=2,
                  output_activation='sigmoid'):
+        
         self.model = RCL( input_shape=input_shape,
                           rec_conv_layers=[
                               [
@@ -34,3 +36,14 @@ class QoIProcessor(object):
                           optimizer=adam(lr=0.001),
                           output_layer=[labels, output_activation]
                        )
+    
+    def window_data(self, x, y=None, window_size=100, overlap=10):
+        
+        idx = window_features(np.arange(x.shape[0]), window_size, overlap)
+        
+        features = x[idx]
+        if y:
+            labels = [y] * idx.shape[0]
+            return features, labels
+        
+        return features
