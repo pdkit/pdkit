f71f86cc7b53bec030998d259221f5711e9bd886
George Roussos <gkroussos@audiencefocus.com>
2020-01-30 12:14:16 +0000

    Cropping option for tapping data.

--- pdkit/finger_tapping_processor.py
+++ pdkit/finger_tapping_processor.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Copyright 2018 Birkbeck College. All rights reserved.
+# Copyright 2020 Birkbeck College. All rights reserved.
 #
 # Licensed under the MIT license. See file LICENSE for details.
 #
-# Author(s): J.S. Pons
+# Author(s): J.S. Pons and George Roussos
 
 import sys
 import logging
@@ -47,7 +47,7 @@ class FingerTappingProcessor:
         except:
             logging.error("Unexpected error on TremorProcessor init: %s", sys.exc_info()[0])
 
-    def frequency(self, data_frame):
+    def frequency(self, data_frame, crop='no'):
         """
             This method returns the number of #taps divided by the test duration
 
@@ -58,11 +58,15 @@ class FingerTappingProcessor:
 
         """
         freq = sum(data_frame.action_type == 1) / data_frame.td[-1]
-        duration = math.ceil(data_frame.td[-1])
+        if crop == 'no':
+            duration = math.ceil(data_frame.td[-1])
+        else:
+            freq = freq - 1
+            duration = math.ceil(data_frame.td[-2])
 
         return freq, duration
 
-    def moving_frequency(self, data_frame):
+    def moving_frequency(self, data_frame, crop='no'):
         """
             This method returns moving frequency
 
@@ -73,17 +77,24 @@ class FingerTappingProcessor:
 
         """
         f = []
-        for i in range(0, (data_frame.td[-1].astype('int') - self.window)):
-            f.append(sum(data_frame.action_type[(data_frame.td >= i) & (data_frame.td < (i + self.window))] == 1) /
-                     float(self.window))
+        if crop == 'no':
+            for i in range(0, (data_frame.td[-1].astype('int') - self.window)):
+                f.append(sum(data_frame.action_type[(data_frame.td >= i) & (data_frame.td < (i + self.window))] == 1) /
+                         float(self.window))
+        else:
+            for i in range(0, (data_frame.td[-2].astype('int') - self.window)):
+                f.append(sum(data_frame.action_type[(data_frame.td >= i) & (data_frame.td < (i + self.window))] == 1) /
+                         float(self.window))
 
         diff_mov_freq = (np.array(f[1:-1]) - np.array(f[0:-2])) / np.array(f[0:-2])
-
-        duration = math.ceil(data_frame.td[-1])
+        if crop == 'no':
+            duration = math.ceil(data_frame.td[-1])
+        else:
+            duration = math.ceil(data_frame.td[-2])
 
         return diff_mov_freq, duration
 
-    def continuous_frequency(self, data_frame):
+    def continuous_frequency(self, data_frame, crop='no'):
         """
             This method returns continuous frequency
 
@@ -93,13 +104,18 @@ class FingerTappingProcessor:
             :rtype cont_freq: float
 
         """
-        tap_timestamps = data_frame.td[data_frame.action_type==1]
-        cont_freq = 1.0/(np.array(tap_timestamps[1:-1])-np.array(tap_timestamps[0:-2]))
-        duration = math.ceil(data_frame.td[-1])
+        if crop == 'no':
+            tap_timestamps = data_frame.td[data_frame.action_type==1]
+            cont_freq = 1.0/(np.array(tap_timestamps[1:-1])-np.array(tap_timestamps[0:-2]))
+            duration = math.ceil(data_frame.td[-1])
+        else:
+            tap_timestamps = data_frame.td[data_frame.action_type==1]
+            cont_freq = 1.0/(np.array(tap_timestamps[1:-2])-np.array(tap_timestamps[0:-3]))
+            duration = math.ceil(data_frame.td[-2])
 
         return cont_freq, duration
 
-    def mean_moving_time(self, data_frame):
+    def mean_moving_time(self, data_frame, crop='no'):
         """
             This method calculates the mean time (ms) that the hand was moving from one target to the next
 
@@ -109,13 +125,18 @@ class FingerTappingProcessor:
             :rtype mmt: float
 
         """
-        diff = data_frame.td[1:-1].values-data_frame.td[0:-2].values
-        mmt = np.mean(diff[np.arange(1,len(diff),2)]) * 1000.0
-        duration = math.ceil(data_frame.td[-1])
+        if crop == 'no':
+            diff = data_frame.td[1:-1].values-data_frame.td[0:-2].values
+            mmt = np.mean(diff[np.arange(1,len(diff),2)]) * 1000.0
+            duration = math.ceil(data_frame.td[-1])
+        else:
+            diff = data_frame.td[1:-2].values-data_frame.td[0:-3].values
+            mmt = np.mean(diff[np.arange(1,len(diff),2)]) * 1000.0
+            duration = math.ceil(data_frame.td[-2])
 
         return mmt, duration
 
-    def incoordination_score(self, data_frame):
+    def incoordination_score(self, data_frame, crop='no'):
         """
             This method calculates the variance of the time interval in msec between taps
 
@@ -125,12 +146,27 @@ class FingerTappingProcessor:
             :rtype is: float
 
         """
-        diff = data_frame.td[1:-1].values - data_frame.td[0:-2].values
+        # if crop == 'no':
+        #     diff = data_frame.td[1:-1].values - data_frame.td[0:-2].values
+        # else:
+        #     diff = data_frame.td[1:-2].values - data_frame.td[0:-3].values
+
+        raise_timestamps = data_frame.td[data_frame.action_type == 1]
+        down_timestamps = data_frame.td[data_frame.action_type == 0]
+        diff = down_timestamps.iloc[1:].values - raise_timestamps.iloc[0:-1].values
+
+        if crop != 'no':
+            diff = diff[:-1]
         inc_s = np.var(diff[np.arange(1, len(diff), 2)], dtype=np.float64) * 1000.0
-        duration = math.ceil(data_frame.td[-1])
+
+        if crop == 'no':
+            duration = math.ceil(data_frame.td[-1])
+        else:
+            duration = math.ceil(data_frame.td[-2])
+
         return inc_s, duration
 
-    def mean_alnt_target_distance(self, data_frame):
+    def mean_alnt_target_distance(self, data_frame, crop='no'):
         """
             This method calculates the distance (number of pixels) between alternate tapping
 
@@ -140,13 +176,21 @@ class FingerTappingProcessor:
             :rtype matd: float
 
         """
-        dist = np.sqrt((data_frame.x[1:-1].values-data_frame.x[0:-2].values)**2+
-                       (data_frame.y[1:-1].values-data_frame.y[0:-2].values)**2)
-        matd = np.mean(dist[np.arange(1,len(dist),2)])
-        duration = math.ceil(data_frame.td[-1])
+        df_down = data_frame[data_frame.action_type==0]
+        if crop == 'no':
+            dist = np.sqrt((df_down.x[1:-1].values-df_down.x[0:-2].values)**2+
+                           (df_down.y[1:-1].values-df_down.y[0:-2].values)**2)
+            matd = np.mean(dist[np.arange(1,len(dist),2)])
+            duration = math.ceil(df_down.td[-1])
+        else:
+            dist = np.sqrt((df_down.x[1:-2].values-df_down.x[0:-3].values)**2+
+                           (df_down.y[1:-2].values-df_down.y[0:-3].values)**2)
+            matd = np.mean(dist[np.arange(1,len(dist),2)])
+            duration = math.ceil(df_down.td[-2])
+
         return matd, duration
 
-    def kinesia_scores(self, data_frame):
+    def kinesia_scores(self, data_frame, crop='no'):
         """
             This method calculates the number of key taps
 
@@ -162,10 +206,12 @@ class FingerTappingProcessor:
         # grouped = tap_timestamps.groupby(pd.TimeGrouper('30u'))
         # return np.mean(grouped.size().values)
         ks = sum(data_frame.action_type == 1)
+        if crop != 'no':
+            ks = ks - 1
         duration = math.ceil(data_frame.td[-1])
         return ks, duration
 
-    def akinesia_times(self, data_frame):
+    def akinesia_times(self, data_frame, crop='no'):
         """
             This method calculates akinesia times, mean dwell time on each key in milliseconds
 
@@ -176,9 +222,14 @@ class FingerTappingProcessor:
             :return duration: test duration (seconds)
             :rtype duration: float
         """
+        if crop != 'no':
+            cropped_df = data_frame.iloc[:-1,:]
+            raise_timestamps = cropped_df.td[cropped_df.action_type == 1]
+            down_timestamps = cropped_df.td[cropped_df.action_type == 0]
+        else:
+            raise_timestamps = data_frame.td[data_frame.action_type == 1]
+            down_timestamps = data_frame.td[data_frame.action_type == 0]
 
-        raise_timestamps = data_frame.td[data_frame.action_type == 1]
-        down_timestamps = data_frame.td[data_frame.action_type == 0]
         if len(raise_timestamps) == len(down_timestamps):
             at = np.mean(down_timestamps.values - raise_timestamps.values)
         else:
@@ -188,10 +239,15 @@ class FingerTappingProcessor:
             else:
                 at = np.mean(down_timestamps.values[:-(len(down_timestamps)-len(raise_timestamps))]
                              - raise_timestamps.values)
-        duration = math.ceil(data_frame.td[-1])
+
+        if crop != 'no':
+            duration = math.ceil(data_frame.td[-2])
+        else:
+            duration = math.ceil(data_frame.td[-1])
+
         return np.abs(at), duration
 
-    def dysmetria_score(self, data_frame):
+    def dysmetria_score(self, data_frame, crop='no'):
         """
             This method calculates accuracy of target taps in pixels
 
@@ -201,12 +257,19 @@ class FingerTappingProcessor:
             :rtype ds: float
 
         """
-        tap_data = data_frame[data_frame.action_type == 0]
-        ds = np.mean(np.sqrt((tap_data.x - tap_data.x_target) ** 2 + (tap_data.y - tap_data.y_target) ** 2))
-        duration = math.ceil(data_frame.td[-1])
+        if crop == 'no':
+            tap_data = data_frame[data_frame.action_type == 0]
+            ds = np.mean(np.sqrt((tap_data.x - tap_data.x_target) ** 2 + (tap_data.y - tap_data.y_target) ** 2))
+            duration = math.ceil(data_frame.td[-1])
+        else:
+            cropped_df = data_frame.iloc[:-1,:]
+            tap_data = cropped_df[cropped_df.action_type == 0]
+            ds = np.mean(np.sqrt((tap_data.x - tap_data.x_target) ** 2 + (tap_data.y - tap_data.y_target) ** 2))
+            duration = math.ceil(cropped_df.td[-1])
+
         return ds, duration
 
-    def extract_features(self, data_frame, pre=''):
+    def extract_features(self, data_frame, pre='', crop='no'):
         """
             This method extracts all the features available to the Finger Tapping Processor class.
 
@@ -219,19 +282,19 @@ class FingerTappingProcessor:
         """
         try:
             if 'x_target' in data_frame.columns:
-                return {pre+'frequency': self.frequency(data_frame)[0],
-                        pre+'mean_moving_time': self.mean_moving_time(data_frame)[0],
-                        pre+'incoordination_score': self.incoordination_score(data_frame)[0],
-                        pre+'mean_alnt_target_distance': self.mean_alnt_target_distance(data_frame)[0],
-                        pre+'kinesia_scores': self.kinesia_scores(data_frame)[0],
-                        pre+'akinesia_times': self.akinesia_times(data_frame)[0],
-                        pre+'dysmetria_score': self.dysmetria_score(data_frame)[0]}
+                return {pre+'frequency': self.frequency(data_frame, crop)[0],
+                        pre+'mean_moving_time': self.mean_moving_time(data_frame, crop)[0],
+                        pre+'incoordination_score': self.incoordination_score(data_frame, crop)[0],
+                        pre+'mean_alnt_target_distance': self.mean_alnt_target_distance(data_frame, crop)[0],
+                        pre+'kinesia_scores': self.kinesia_scores(data_frame, crop)[0],
+                        pre+'akinesia_times': self.akinesia_times(data_frame, crop)[0],
+                        pre+'dysmetria_score': self.dysmetria_score(data_frame, crop)[0]}
             else:
-                return {pre+'frequency': self.frequency(data_frame)[0],
-                        pre+'mean_moving_time': self.mean_moving_time(data_frame)[0],
-                        pre+'incoordination_score': self.incoordination_score(data_frame)[0],
-                        pre+'mean_alnt_target_distance': self.mean_alnt_target_distance(data_frame)[0],
-                        pre+'kinesia_scores': self.kinesia_scores(data_frame)[0],
-                        pre+'akinesia_times': self.akinesia_times(data_frame)[0]}                
+                return {pre+'frequency': self.frequency(data_frame, crop)[0],
+                        pre+'mean_moving_time': self.mean_moving_time(data_frame, crop)[0],
+                        pre+'incoordination_score': self.incoordination_score(data_frame, crop)[0],
+                        pre+'mean_alnt_target_distance': self.mean_alnt_target_distance(data_frame, crop)[0],
+                        pre+'kinesia_scores': self.kinesia_scores(data_frame, crop)[0],
+                        pre+'akinesia_times': self.akinesia_times(data_frame, crop)[0]}
         except:
             logging.error("Error on FingerTappingProcessor process, extract features: %s", sys.exc_info()[0])
