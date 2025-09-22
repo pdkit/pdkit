1059a4ca0ec964b8d5abc98ed8a2f33e78796ff2
PDkit Project <37222457+pdkit@users.noreply.github.com>
2025-09-22 14:51:04 +0100

    Update setup.py
    Updating set up for 3.11

--- setup.py
+++ setup.py
@@ -1,38 +1,38 @@
 #!/usr/bin/env python3
-# Copyright 2022 Birkbeck College. All rights reserved.
+# Copyright 2025 Birkbeck College. All rights reserved.
 #
 # Licensed under the MIT license. See file LICENSE for details.
 #
-# Author(s): J.S. Pons and George Roussos
+# Author(s): J.S. Pons, Alex Noble and George Roussos
 # -*- coding: utf-8 -*-
 
 import os
 import re
 import sys
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.command.install import install
 
 
 def get_version():
-    with open(os.path.join("pdkit", "_version.py")) as f:
+    with open(os.path.join("pdkit", "_version.py"), encoding="utf-8") as f:
         return re.search(r'__version__ = "([^"]+)"', f.read()).group(1)
 
 def readme():
-    with open('readme-pypi.rst') as f:
+    with open("readme-pypi.rst", encoding="utf-8") as f:
         return f.read()
-    
+
 
 # circleci.py version
 VERSION = get_version()
 
+
 class VerifyVersionCommand(install):
     """Custom command to verify that the git tag matches our version"""
-    description = 'verify that the git tag matches our version'
+    description = "verify that the git tag matches our version"
 
     def run(self):
-        tag = os.getenv('CIRCLE_TAG')
-
+        tag = os.getenv("CIRCLE_TAG")
         if tag != VERSION:
             info = "Git tag: {0} does not match the version of this app: {1}".format(
                 tag, VERSION
@@ -41,42 +41,75 @@ class VerifyVersionCommand(install):
 
 
 setup(
-    name='pdkit',
+    name="pdkit",
     version=VERSION,
-    description='The Parkinson`s Disease Data Science Toolkit',
-    url='git+https://github.com/pdkit/pdkit.git#egg=proj',
+    description="The Parkinson`s Disease Data Science Toolkit",
+    url="https://github.com/pdkit/pdkit",
+    project_urls={
+        "Source": "https://github.com/pdkit/pdkit",
+        "Tracker": "https://github.com/pdkit/pdkit/issues",
+    },
     long_description=readme(),
-    keywords='parkinson`s disease',
-    author='PDkit Project members',
-    author_email='g.roussos@bbk.ac.uk',
-    license='MIT',
-    packages=['pdkit'],
-    install_requires=[
-        "future==1.0.0",
-        "keras==3.10.0",
-        "matplotlib==3.10.3",
-        'numpy==1.23.5',
-        "pandas==2.3.0",
-        "pandas_validator==0.5.0",
-        "praat-parselmouth==0.4.6",
-        "scikit_learn==1.7.0",
-        "scipy==1.15.3",
-        "soundfile==0.13.1",
-        "stumpy==1.13.0",
-        "tqdm==4.67.1",
-        "tsfresh==0.21.0",
-    ],
+    long_description_content_type="text/x-rst",
+    keywords="parkinson`s disease",
+    author="PDkit Project members",
+    author_email="g.roussos@bbk.ac.uk",
+    license="MIT",
+    packages=find_packages(exclude=("tests", "docs")),
+    include_package_data=True,
     zip_safe=False,
+
+    # ---- Python 3.11 support ----
+    python_requires=">=3.11,<3.14",
+
+    install_requires=[
+        # Core scientific stack (compatible with Python 3.11)
+        # NumPy 1.23.5+ has official Python 3.11 support; keep <2 to avoid
+        # downstream packages that may not yet be NumPy 2–ready.
+        "numpy>=1.23.5,<2.0",            # Py3.11 supported from 1.23.5
+        "pandas>=2.1,<2.4",              # pandas supports Py 3.11 (2.1–2.3)
+        "scipy>=1.10",                   # SciPy supports Py 3.11
+
+        # ML/feature extraction
+        "scikit-learn>=1.3,<1.8",        # modern sklearn w/ Py3.11 wheels
+        "tsfresh>=0.21.0",               # fixes for SciPy>=1.15 and NumPy>=2
+        "stumpy>=1.13.0",                # depends on numba; Py3.11 now supported
+
+        # Plotting / utils
+        "matplotlib>=3.7",               # Matplotlib follows NEP29; Py3.11 OK
+        "tqdm>=4.65,<5",
+
+        # Audio + speech
+        "soundfile>=0.12,<0.14",         # 0.13.x tested on Py3.11
+        "praat-parselmouth>=0.4.6",
+
+        # DL front-end (leave backend choice to user; TF/JAX/PyTorch optional)
+        "keras>=3,<4",
+
+        # Legacy helper; harmless on Py3 but kept for compatibility
+        "future>=0.18.3",
+    ],
+
+    # Optional replacement for the unmaintained pandas_validator
+    extras_require={
+        "validation": [
+            "pandera[pandas]>=0.20"      # modern DataFrame validation
+        ]
+    },
+
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
     cmdclass={
-        'verify': VerifyVersionCommand,
-    }
+        "verify": VerifyVersionCommand,
+    },
 )
